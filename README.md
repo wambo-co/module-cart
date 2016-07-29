05d11eb3ce1af7e9b616e6acbb0bc996430545a9
Andreas Koch <andyk7@gmail.com>
2016-07-29 22:14:35 +0200

    Initial commit 🛍

---
+++ README.md
@@ -0,0 +1,17 @@
+# Wambo Cart
+
+A shopping cart for Wambo shops
+
+**wambo/module-cart** provides shopping cart functionality for Wambo shops.
+
+## Features
+
+None yet.
+
+## Roadmap
+
+1. Cart persistence
+2. Cart API (Add, Update, Remove)
+3. Mini cart
+4. Cart page
+
---
+++ src/Registration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wambo\Cart;
+
+/**
+ * Class Registration registers the cart module in the Wambo app.
+ *
+ * @package Wambo\Cart
+ */
+class Registration implements ModuleBootstrapInterface
+{
+    /**
+     * Register the cart module.
+     *
+     * @param App $app
+     */
+    public function __construct(App $app)
+    {
+        $this->configureDI($app);
+        $this->registerRoutes($app);
+    }
+
+    /**
+     * Register routes in the slim app.
+     *
+     * @param App $app
+     */
+    private function registerRoutes(App $app)
+    {
+    }
+
+    /**
+     * Configure the dependency injection container
+     *
+     * @param App $app
+     */
+    private function configureDI(App $app)
+    {
+        /** @var \DI\Container $container */
+        $container = $app->getContainer();
+    }
+}
